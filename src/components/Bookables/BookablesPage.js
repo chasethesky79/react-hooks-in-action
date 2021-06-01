fac2dbf207d0b104ce128ed2569ebb94228f476c
Bharath Seshadri <bharathseshadri@kukubara7.local>
2021-05-31 22:12:43 -0500

    Added more CSS styling to bookables page

--- src/components/Bookables/BookablesPage.js
+++ src/components/Bookables/BookablesPage.js
@@ -1,7 +1,14 @@
+import data from '../../static.json';
+
 export default function BookablesPage() {
+    const group = 'Rooms';
+    const { bookables } = data;
+    const bookablesMatchingGroup = bookables.filter(bookable => bookable.group === group);
+    const bookableIndex = 1;
     return (
-        <main className="bookables-page">
-            <p>Bookables!</p>
-        </main>
+        <ul className='items-list-nav'>
+            {bookablesMatchingGroup.map((item,i) => <li key={i} className={i === bookableIndex ? 'selected' : null}>
+                <button className='btn'>{item.title}</button></li>)}
+        </ul>
     )
 }
