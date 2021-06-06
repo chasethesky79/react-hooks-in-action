ec5dae67b6fd208476ef8d36cc50797f85b785a9
Bharath Seshadri <bharathseshadri@kukubara7.local>
2021-06-06 13:39:47 -0500

    Implemented the group selection drop-down

--- src/components/Bookables/BookablesList.js
+++ src/components/Bookables/BookablesList.js
@@ -3,14 +3,30 @@ import data from '../../static.json';
 import { FaArrowRight } from 'react-icons/fa';
 
 export default function BookablesList() {
-    const group = 'Rooms';
     const { bookables } = data;
-    const bookablesMatchingGroup = bookables.filter(bookable => bookable.group === group);
+    const groups = bookables.reduce((acc, element) => {
+      if (!acc.includes(element.group)){
+          acc = [...acc, element.group]
+      }
+      return acc;
+    }, []);
     const [bookableIndex, setBookableIndex] = useState(1);
+    const [group, setGroup] = useState(groups[0]);
     const handleBookableSelection = (index) => setBookableIndex(index);
+    const handleGroupSelection = ({ target: { value }}) => {
+        setBookableIndex(0);
+        setGroup(value);
+    };
+    const bookablesMatchingGroup = bookables.filter(bookable => bookable.group === group);
     const handleNextSelection = () => setBookableIndex((index) => index === bookablesMatchingGroup.length - 1 ? 0 : index + 1);
     return (
         <ul className='items-list-nav'>
+            <select
+                value={group}
+                onChange={handleGroupSelection}
+            >
+              {groups.map((group, index) => <option key={index} value={group} >{group}</option>)}
+            </select>
             {bookablesMatchingGroup.map((item,i) => <li key={i} className={i === bookableIndex ? 'selected' : null}>
                 <button className='btn' onClick={() => handleBookableSelection(i)}>{item.title}</button></li>)}
             <button onClick={handleNextSelection} className='nextBtn'><FaArrowRight/><span>&nbsp;Next</span></button>
