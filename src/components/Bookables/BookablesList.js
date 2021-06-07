e545fc524e4f2d4a01ed51b9ecf51819a0ca3c73
Bharath Seshadri <bharathseshadri@kukubara7.local>
2021-06-06 21:34:46 -0500

    Added CSS styling to the layout of items

--- src/components/Bookables/BookablesList.js
+++ src/components/Bookables/BookablesList.js
@@ -20,16 +20,26 @@ export default function BookablesList() {
     const bookablesMatchingGroup = bookables.filter(bookable => bookable.group === group);
     const handleNextSelection = () => setBookableIndex((index) => index === bookablesMatchingGroup.length - 1 ? 0 : index + 1);
     return (
-        <ul className='items-list-nav'>
-            <select
-                value={group}
-                onChange={handleGroupSelection}
-            >
-              {groups.map((group, index) => <option key={index} value={group} >{group}</option>)}
-            </select>
-            {bookablesMatchingGroup.map((item,i) => <li key={i} className={i === bookableIndex ? 'selected' : null}>
-                <button className='btn' onClick={() => handleBookableSelection(i)}>{item.title}</button></li>)}
-            <button onClick={handleNextSelection} className='nextBtn'><FaArrowRight/><span>&nbsp;Next</span></button>
-        </ul>
+        <div class="container">
+           <div className='bookables-list'>
+                <select
+                    value={group}
+                    onChange={handleGroupSelection}>
+                    {groups.map((group, index) => <option key={index} value={group} >{group}</option>)}
+                </select>
+               <ul className='items-list-nav'>
+                    {bookablesMatchingGroup.map((item,i) => <li key={i} className={i === bookableIndex ? 'selected' : null}>
+                        <button className='btn' onClick={() => handleBookableSelection(i)}>{item.title}</button></li>)}
+                    <button onClick={handleNextSelection} className='nextBtn'><FaArrowRight/><span>&nbsp;Next</span></button>
+                </ul>
+            </div>
+            <div class='bookable-details-section'>
+                <div class='bookable-details'>
+                    <h3>Projector</h3>
+                    <span>Show Details</span>
+                </div>
+            </div>
+        </div>
+        
     )
 }
