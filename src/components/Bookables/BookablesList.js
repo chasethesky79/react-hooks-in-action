0148229112150cd08d5d967bef5bd6e5304be394
Bharath Seshadri <bharathseshadri@kukubara7.local>
2021-07-11 18:52:47 -0500

    Added CSS for list items

--- src/components/Bookables/BookablesList.js
+++ src/components/Bookables/BookablesList.js
@@ -3,7 +3,7 @@ import data from '../../static.json';
 import { FaArrowRight } from 'react-icons/fa';
 
 export default function BookablesList() {
-    const { bookables } = data;
+    const { bookables, sessions, days } = data;
     const groups = bookables.reduce((acc, element) => {
       if (!acc.includes(element.group)){
           acc = [...acc, element.group]
@@ -12,12 +12,18 @@ export default function BookablesList() {
     }, []);
     const [bookableIndex, setBookableIndex] = useState(1);
     const [group, setGroup] = useState(groups[0]);
-    const handleBookableSelection = (index) => setBookableIndex(index);
-    const handleGroupSelection = ({ target: { value }}) => {
-        setBookableIndex(0);
-        setGroup(value);
-    };
+    const [showDetails, setShowDetails] = useState(true);
     const bookablesMatchingGroup = bookables.filter(bookable => bookable.group === group);
+    const [bookable, setBookable] = useState(bookablesMatchingGroup[bookableIndex]);
+    const handleBookableSelection = (index) => {
+         setBookableIndex(index);
+         setBookable(bookablesMatchingGroup[index]);
+    }
+    const handleGroupSelection = ({ target: { value }}) => {
+        setGroup(value);
+        setBookableIndex(0);
+        setBookable(bookablesMatchingGroup[0]);
+    };
     const handleNextSelection = () => setBookableIndex((index) => index === bookablesMatchingGroup.length - 1 ? 0 : index + 1);
     return (
         <div class="container">
@@ -34,10 +40,23 @@ export default function BookablesList() {
                 </ul>
             </div>
             <div class='bookable-details-section'>
-                <div class='bookable-details'>
+                <div className='bookable-details-header'>
                     <h3>Projector</h3>
-                    <span>Show Details</span>
+                    <span><label><input type='checkbox' checked={showDetails} onChange={() => setShowDetails(!showDetails)}/>Show Details</label></span>
                 </div>
+                {showDetails && 
+                 <div className='bookable-details-body'>
+                   <h3>{bookable.notes}</h3>
+                   <h3>Availability</h3>
+                   <div className='bookable-details-info'>
+                    <ul>
+                       {bookable.days.sort().map(day => <li key={day}>{days[day]}</li>)}
+                    </ul>
+                    <ul>
+                       {bookable.sessions.sort().map(session => <li key={session}>{sessions[session]}</li>)}
+                    </ul>               
+                   </div>
+                </div>}
             </div>
         </div>
         
