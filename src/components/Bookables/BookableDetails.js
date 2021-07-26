aaa0c8cd747118a52865f229bf67086bf7633918
Bharath Seshadri <bharathseshadri@kukubara7.local>
2021-07-25 19:52:13 -0500

    Reformatted cbookable details component code

--- src/components/Bookables/BookableDetails.js
+++ src/components/Bookables/BookableDetails.js
@@ -4,23 +4,23 @@ export default function BookableDetails({ bookable, showDetails, handleShowDetai
     const { sessions, days } = data;
     return (
         <div className='bookable-details-section'>
-                <div className='bookable-details-header'>
-                    <h3>{bookable.title}</h3>
-                    <span><label><input type='checkbox' checked={showDetails} onChange={handleShowDetailsToggled}/>Show Details</label></span>
-                </div>
-                {showDetails && 
-                 <div className='bookable-details-body'>
-                   <h3>{bookable.notes}</h3>
-                   <h3>Availability</h3>
-                   <div className='bookable-details-info'>
-                    <ul>
-                       {bookable.days.sort().map(day => <li key={day}>{days[day]}</li>)}
-                    </ul>
-                    <ul>
-                       {bookable.sessions.sort().map(session => <li key={session}>{sessions[session]}</li>)}
-                    </ul>               
-                   </div>
-                </div>}
+            <div className='bookable-details-header'>
+                <h3>{bookable.title}</h3>
+                <span><label><input type='checkbox' checked={showDetails} onChange={handleShowDetailsToggled}/>Show Details</label></span>
             </div>
+            {showDetails && 
+                <div className='bookable-details-body'>
+                <h3>{bookable.notes}</h3>
+                <h3>Availability</h3>
+                <div className='bookable-details-info'>
+                <ul>
+                    {bookable.days.sort().map(day => <li key={day}>{days[day]}</li>)}
+                </ul>
+                <ul>
+                    {bookable.sessions.sort().map(session => <li key={session}>{sessions[session]}</li>)}
+                </ul>               
+                </div>
+            </div>}
+        </div>
     )
 }
